f08281bccb13ec01e9b4ffde99ecc31d7300b820
Takamasa Oshikiri <mail@oshikiri.org>
2024-02-03 19:23:42 +0900

    Add bug test case to rectangle.test.js

--- test/rectangle.test.js
+++ test/rectangle.test.js
@@ -1,9 +1,15 @@
 const { loadDocument } = require("./utils");
 
+const document = loadDocument("./demo/rectangle/myd3.html");
+
 test("myd3 inserts blue 50x20 rectangle", () => {
-  const document = loadDocument("./demo/rectangle/myd3.html");
   const rect = document.querySelector("#chart > svg > g > rect");
   expect(rect.getAttribute("fill")).toBe("blue");
   expect(rect.getAttribute("width")).toBe("50");
   expect(rect.getAttribute("height")).toBe("20");
+});
+
+test("[bug] myd3 inserts an xhtml rect element", () => {
+  const rect = document.querySelector("#chart > svg > g > rect");
+  expect(rect.namespaceURI).toBe("http://www.w3.org/1999/xhtml");
 });
